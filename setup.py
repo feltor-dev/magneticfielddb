2722b3782ba85073647018050bc0e3289f63c435
Matthias <mattwi@fysik.dtu.dk>
2024-11-29 13:37:45 +0100

    Apply ruff formatter

--- setup.py
+++ setup.py
@@ -12,17 +12,14 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/feltor-dev/magneticfielddb",
-    #packages=setuptools.find_packages(),
+    # packages=setuptools.find_packages(),
     pymodules=["magneticfielddb"],
     include_package_data=True,
     #    Accept all data files and directories matched by MANIFEST.in.
-    package_data={
-        "": ["data/*"]
-    },
+    package_data={"": ["data/*"]},
     #    Specify additional patterns to match files that may or may not be matched by MANIFEST.in or found in source control.
     exclude_package_data={"": ["polynomial_field.py"]},
     #    Specify patterns for data files and directories that should not be included when a package is installed, even if they would otherwise have been included due to the use of the preceding options.
-
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -31,7 +28,7 @@ setuptools.setup(
         "Topic :: Scientific simulations :: Libraries",
         "Topic :: Utilities",
     ],
-    python_requires='>=3.6',
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest']
+    python_requires=">=3.6",
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
 )
--- test_magneticfielddb.py
+++ test_magneticfielddb.py
@@ -1,27 +1,26 @@
-import pytest
 import magneticfielddb as mag
 import os.path
 
 # Run with pytest -s . to see stdout output
 
-def test_selection () :
-    print ( "TEST SELECTION")
-    m = mag.select( "COMPASS/compass_1X.json")
-    print( m)
-    m = mag.select( "COMPASS", "compass_1X.json")
-    print( m)
-    m = mag.select( os.path.join("COMPASS", "compass_1X.json"))
-    print( m)
 
-def test_list() :
-    print ( "TEST LIST")
+def test_selection():
+    print("TEST SELECTION")
+    m = mag.select("COMPASS/compass_1X.json")
+    print(m)
+    m = mag.select("COMPASS", "compass_1X.json")
+    print(m)
+    m = mag.select(os.path.join("COMPASS", "compass_1X.json"))
+    print(m)
 
-    #test listing
-    for p in mag.files() :
-        print( p )
-    #test if list can be selected
-    for p in mag.files() :
+
+def test_list():
+    print("TEST LIST")
+
+    # test listing
+    for p in mag.files():
+        print(p)
+    # test if list can be selected
+    for p in mag.files():
         m = mag.select(p)
         print(m)
-
-
